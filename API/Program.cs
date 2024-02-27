8b75d814fe160f75d03b5e7f4bc8f4b243b80e51
Adam M <whiskypo082@gmail.com>
2024-02-26 21:43:43 -0600

    Added http/https connection to the API server.

--- API/Program.cs
+++ API/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddDbContext<DataContext>(option =>
     option.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")); 
 });
 
+builder.Services.AddCors(); 
 
 var app = builder.Build();
 
@@ -19,6 +20,9 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.UseCors(builder => builder.AllowAnyHeader()
+                              .AllowAnyHeader()
+                              .WithOrigins("http://localhost:4200")); 
 app.MapControllers();
 
 app.Run();
