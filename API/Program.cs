51a121bef6be602460b11ffa838e47b9c9925f24
Adam M <whiskypo082@gmail.com>
2024-03-01 21:29:59 -0600

    Added Extension folder to put some of the Program.cs builde services in. Fixed Token auth in program.cs.

--- API/Program.cs
+++ API/Program.cs
@@ -5,38 +5,20 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(option => 
-{ 
-    option.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")); 
-});
-
-builder.Services.AddCors(); 
-builder.Services.AddScoped<ITokenService, TokenService>(); 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(option => 
-    {
-        option.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true, 
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"])), 
-            ValidateIssuer = false, 
-            ValidateAudience = false
-        };
-    }); 
-
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.UseCors(builder => builder.AllowAnyHeader()
                               .AllowAnyHeader()
