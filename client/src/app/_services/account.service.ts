37e201903072655834e87c39db753ba31a2fa854
Adam M <whiskypo082@gmail.com>
2024-03-15 20:38:03 -0500

    Added Admin section. Added Admin Roles.

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -42,6 +42,10 @@ export class AccountService {
   
   setCurrentUser(user: User)
   {
+    user.roles = [];
+    const roles = this.getDecodedToken(user.token).role;
+    Array.isArray(roles) ? user.roles = roles : user.roles.push(roles);
+    localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
 
@@ -49,5 +53,9 @@ export class AccountService {
   {
     localStorage.removeItem('user'); 
     this.currentUserSource.next(null); 
+  }
+
+  getDecodedToken(token: string) {
+    return JSON.parse(atob(token.split('.')[1]));
   }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -26,6 +26,11 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
 import { DatePickerComponent } from './_forms/date-picker/date-picker.component';
 import { TextInputComponent } from './_forms/text-input/text-input.component';
 import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
+import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
+import { PhotoManagementComponent } from './admin/photo-management/photo-management.component';
+import { UserManagementComponent } from './admin/user-management/user-management.component';
+import { RolesModalComponent } from './modals/roles-modal/roles-modal.component';
+import { HasRoleDirective } from './directives/has-role.directive';
 
 @NgModule({
   declarations: [
@@ -45,6 +50,11 @@ import { MemberMessagesComponent } from './members/member-messages/member-messag
     PhotoEditorComponent,
     TextInputComponent,
     DatePickerComponent,
+    AdminPanelComponent,
+    PhotoManagementComponent,
+    UserManagementComponent,
+    RolesModalComponent,
+    HasRoleDirective,
   ],
   imports: [
     BrowserModule,
